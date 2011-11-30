c6e5f6363e494ec89766a69b77497278ba4c50ab
nicholas.zhaoyu@gmail.com <nicholas.zhaoyu@gmail.com@35f81436-e3ac-c436-dc8e-f9662aa848d5>
2011-11-30 07:32:07 +0000

    MOD: shit.

--- src/com/nich01as/air/BeijingAirActivity.java
+++ src/com/nich01as/air/BeijingAirActivity.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.UUID;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -24,8 +23,6 @@ import android.net.http.AndroidHttpClient;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Parcelable;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -85,6 +82,26 @@ public class BeijingAirActivity extends Activity {
         mAdLayout.addView(mAdView);
         mApp = (AirApp) getApplication();
         mDesText = (TextView) findViewById(R.id.air_des);
+//        System.setProperty("weibo4j.oauth.consumerKey", Weibo.CONSUMER_KEY);
+//        System.setProperty("weibo4j.oauth.consumerSecret", Weibo.CONSUMER_SECRET);
+//        Weibo weibo = new Weibo();
+//        //weibo.setOAuthConsumer("2173128732", "4afb9d8a45e57a3ab60f405fd02beb0c");
+//        
+//        try {
+//            Oau
+//            Log.d("Air", oauth.);
+//            weibo.setOAuthAccessToken("2428f8c46120a7fce61cd94561529070", "28a054ad9aa57d9f3d82cc6094191123");
+//            //Log.d("Air", weibo.getOAuthRequestToken("http://nich01as.com").getAuthenticationURL());
+//            //Log.d("Air", weibo.getOAuthRequestToken().getAccessToken(pin));
+//            AccessToken accessToken = weibo.getOAuthRequestToken().getAccessToken("613997");
+//            Log.d("Air", accessToken.getToken() + " : " + accessToken.getTokenSecret());
+//            //weibo.setOAuthAccessToken("6a00e3c90fc5c92d2b2240d95f466713", "6717a467d5a9f5eda868c57fbeb43e6b");
+//            weibo.setToken(accessToken);
+//            User user = weibo.showUser("beijingairpm25");
+//            Log.d("air", "user id is " + user.getId());
+//        } catch (WeiboException e) {
+//            e.printStackTrace();
+//        }
    }
     
     private class UpdateChecker extends AsyncTask<Void, Void, AirStatus> {
